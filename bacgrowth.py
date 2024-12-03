0692d337da43665d4d86d32a44cc464cd34e8653
pnada <melove.mariokart@gmail.com>
2024-12-03 17:03:11 +1100

    Adding plotext graphing functionality

--- bacgrowth.py
+++ bacgrowth.py
@@ -1,6 +1,7 @@
 #A barebones program to estimate bacterial exponential growth times to harvest (primarily for Tree Lab comp cell preps)
 
 import math
+import numpy as np
 
 #Using formula A=P0*e^(rt)
 
@@ -34,5 +35,31 @@ endpointTimeLower = round(math.log(endpointDensityLower/originalDensity)/rConsta
 endpointTimeUpper = round(math.log(endpointDensityUpper/originalDensity)/rConstant)
 hourMinuteLower = '{:02d}:{:02d}'.format(*divmod(endpointTimeLower, 60))
 hourMinuteUpper = '{:02d}:{:02d}'.format(*divmod(endpointTimeUpper, 60))
+print("__________________________")
 print(f"Expected time for OD of {endpointDensityLower}: {endpointTimeLower} minutes, or {hourMinuteLower} ")
 print(f"Expected time for OD of {endpointDensityUpper}: {endpointTimeUpper} minutes, or {hourMinuteUpper} ")
+
+
+
+try:
+    import plotext as plt
+    # Provides a function for exponential growth
+    def finalDensity(c, r, t):
+         return(c * math.exp(r * t))
+    
+    # Use plotext to generate a graph of the exponential growth curve
+    x = range(0, round(endpointTimeUpper+120))
+    y = [finalDensity(originalDensity, rConstant, time) for time in x]
+    plt.plot(y, marker = "dot")
+    plt.vline(endpointTimeLower)
+    plt.hline(endpointDensityLower)
+    plt.vline(endpointTimeUpper)
+    plt.hline(endpointDensityUpper)
+    plt.title("Growth curve")
+    plt.xlabel("Minutes")
+    plt.show()
+    
+    #If plotext is not installed:
+except ModuleNotFoundError:
+        print("No graph generated as plotext is not installed")
+        print("conda install conda-forge::plotext or pip install plotext==5.3.2")
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@ This is a super barebones program written in 2 hours to estimate bacterial optic
 I recommend checking the OD at around 75% of the time that the program estimates it to ensure the cultures do not overgrow (might add this calculation in the future).\
 The program is based on the exponential growth formula: A = P<sub>0</sub>e<sup>rt</sup>, where A = current OD, P<sub>0</sub> = original OD, r = growth constant, and t = time.\
 It calcualtes the r constant using the intermediate OD measurement and uses it to calculate the time t.
+
+Plotext is an optional dependency for the generation of a growth curve graph. You can install it with either:\
+conda install conda-forge::plotext or pip install plotext==5.3.2
